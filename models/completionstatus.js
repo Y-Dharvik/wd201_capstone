fc8ea6b77e3d5d1b46cca069139a306178e70cdb
Y-Dharvik <21951a0536@iare.ac.in>
2024-02-05 19:59:01 +0530

    added foreign key constraint

--- models/completionstatus.js
+++ models/completionstatus.js
@@ -9,6 +9,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      completionStatus.belongsTo(models.Page, {
+        foreignKey: "pageId",
+      });
     }
   }
   completionStatus.init(
